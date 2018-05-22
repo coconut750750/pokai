440919660b83a69789b67a66e8ced5de3f3d9813
Brandon Wang <sourbrandon@gmail.com>
2018-05-22 13:42:35 -0500

    added test files

---
+++ test_card.py
@@ -0,0 +1,36 @@
+"""
+Testing module for card.py
+
+run with:
+pytest test_card.py -vs
+
+-v : verbose
+-s : switch (allows printing)
+"""
+
+import card
+
+class TestCard(object):
+    """
+    Test class for testing cards
+    """
+
+    def test_card_display_6(self):
+        """Testing card displays"""
+        assert str(card.Card(card.VALUES[3], card.SUITS[2])) == '♠6'
+
+    def test_card_display_Q(self):
+        """Testing card displays"""
+        assert str(card.Card(card.VALUES[9], card.SUITS[1])) == '♦Q'
+
+    def test_card_display_invalid_joker(self):
+        """Testing if card recognizes invalid jokers"""
+        assert str(card.Card("Z", card.SUITS[3])) == 'INVALID'
+
+    def test_card_display_big_joker(self):
+        """Testing if card recognizes big joker"""
+        assert str(card.Card("Z", 1)) == 'JOKER'
+
+    def test_card_display_small_joker(self):
+        """Testing if card recognizes big joker"""
+        assert str(card.Card("Z", 0)) == 'joker'
---
+++ test_hand.py
@@ -0,0 +1,17 @@
+"""
+Testing module for hand.py
+
+run with:
+pytest test_card.py -vs
+
+-v : verbose
+-s : switch (allows printing)
+"""
+
+import hand
+
+class TestHand(object):
+    """
+    Test class for testing cards
+    """
+
